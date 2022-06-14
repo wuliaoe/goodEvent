c097a859b72929f515363b0828ad2ca9aca13592
栢衣 <792916507@qq.com>
2022-06-14 21:56:23 +0800

    完成主页功能开发，如获取用户信息、头像渲染、退出登录等功能等等

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,6 +2,27 @@
 //这个函数用来统一拼接url的前缀地址，然后发送请求的时候只需要输入后缀即可
 
 $.ajaxPrefilter(function (options) {
-    //提取后缀，然后拼接前缀
+    //全局统一提取后缀，然后拼接前缀
     options.url = 'http://www.liulongbin.top:3008' + options.url
+
+    //全局统一配置headers,如果url路径里面有my则放添加用户请求头信息
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局挂载一个complete 只要请求没有成功获取用户信息则清除token且跳转到登录页面
+    // 无论请求成功还是失败，ajax请求都会调用complete函数
+    options.complete = function (res) {
+        // console.log('@complete:', res);
+        //通过log我们得出res.responseJSON是获取后的信息
+        // const data = res.responseJSON
+        //通过接口测试得出失败返回的是code = 1 message=身份认证失败！
+        //这里加一个if判断如果失败则跳转到登录页面且强制删除token
+        if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,67 @@
+$(function () {
+    getUserInfor()
+})
+
+
+function getUserInfor() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            // console.log(res);
+            if (res.code !== 0) {
+                return layui.layer.msg('获取用户信息失败啦！！')
+            }
+            // 获取信息成功后则开始渲染头像等信息，由于步骤过多这里封装一个函数
+            readUserInfor(res.data)
+            layui.layer.msg(res.message)
+        },
+        // 无论请求成功还是失败 都会调用complete函数
+        // complete: function (res) {
+        //     // console.log('@complete:', res);
+        //     //通过log我们得出res.responseJSON是获取后的信息
+        //     const data = res.responseJSON
+        //     //通过接口测试得出失败返回的是code = 1 message=身份认证失败！
+        //     //这里加一个if判断如果失败则跳转到登录页面且强制删除token
+        //     if (data.code === 1 && data.message === '身份认证失败！') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+
+// 渲染用户头像函数
+function readUserInfor(user) {
+    const name = user.nickname || user.username
+    $('#welcome').html(`${name}-欢迎宁！`)
+
+    // 开始渲染头像
+    const picSrc = user.user_pic
+    if (picSrc !== null) {
+        $('.layui-nav-img').attr('src', picSrc).show()
+        $('.user-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        //  将名字里的第一个元素大写渲染出来
+        const first = name[0].toUpperCase()
+        $('.user-avatar').html(first).show()
+    }
+}
+
+//退出事件按钮功能编写
+
+$('#btn_BackLogin').click(() => {
+    // layui.layer.msg('点击退出按钮成功')
+    layer.confirm('宁确定要退出嘛？', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        //首先删除locostrage的缓存token
+        localStorage.removeItem('token') //清除指定
+        // localStorage.clear() //清除所有
+
+        //然后返回到浏览器登录页面
+        location.href = '/login.html'
+
+        layer.close(index);
+    });
+})
